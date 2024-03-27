179bb2f0ed412deff87a4484931959bf942c3edb
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-03-27 17:02:08 +0100

    base projet

---
+++ Forum_Holder_Gromangin/pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Erreur 404</h1>
+        <p>La page demandée n'existe pas.</p>
+        <img src="https://http.cat/404" alt="Erreur 404">
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Erreur 404',
+        };
+    },
+};
+</script>
---
+++ Forum_Holder_Gromangin/pages/forum/[id].vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>{{ message.title }}</h1>
+        <p>{{ message.content }}</p>
+    </div>
+</template>
+
+<script>
+</script>
