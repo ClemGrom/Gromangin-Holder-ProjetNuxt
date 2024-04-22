d39e138bb196dea158f309bc9f5ca73190430f3e
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-22 22:33:16 +0200

    ajout de sujet dans la page sujet

--- Forum_Holder_Gromangin/pages/forum/[id].vue
+++ Forum_Holder_Gromangin/pages/forum/[id].vue
@@ -1,31 +1,25 @@
 <template>
-    <v-container color="cyan-darken-3 lighten-5">
-      <v-row>
-        <v-col>
-          <h1 class="text-h4 mb-4">Sujets</h1>
-          <v-card class="pa-3 mb-4" v-if="forum && forum.length > 0">
-            <p>Voici les sujets du forum: {{forum[0].name}}</p>
-          </v-card>
-          <v-card class="pa-3 mb-4" v-else>
-            <p>Aucun forum trouvé</p>
-          </v-card>
-          <v-card class="pa-3 mb-4" color="cyan-darken-3 lighten-5">
-            <p>Nombre de Sujet {{ this.sujets.length }}</p>
-          </v-card>
-          <v-list>
+  <input type="text" v-model="name" placeholder="Nom du sujet" />
+  <v-btn @click="addSujet">Ajouter un sujet</v-btn>
+    <div>
+        <h1>Sujets</h1>
+        <p v-if="forum && forum.length > 0">Voici les sujets du forum: {{forum[0].name}}</p>
+        <p v-else>Aucun forum trouvé</p>
+        <p>Nombre de Sujet {{ this.sujets.length }}</p>
+        <v-list>
             <v-list-item v-for="sujet in sujets" :key="sujet.id">
-              <v-list-item-content>
-                <v-list-item-title>
-                  <nuxt-link :to="`/forum/sujet/${sujet.id}`">{{ sujet.name }}</nuxt-link>
-                </v-list-item-title>
-                <div>{{ sujet.created }}</div>
-              </v-list-item-content>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        <nuxt-link :to="`/forum/sujet/${sujet.id}`">{{ sujet.name }}</nuxt-link>
+                    </v-list-item-title>
+                    <div>{{ sujet.created }}</div>
+                </v-list-item-content>
             </v-list-item>
-          </v-list>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+        </v-list>
+        
+    </div>
+</template>
+
 <script>
 
 // const addFav = async (id) => {
@@ -45,6 +39,7 @@ export default {
         return {
             forum: null,
             sujets: [],
+            name: '',
         };
     },
     async mounted() {
@@ -52,8 +47,6 @@ export default {
         this.forum = data.forums;
         const sujets = await this.fetchSujets(this.$route.params.id);
         this.sujets = sujets.sujets;
-
-        const { data: posts } = await useFetch(`http://localhost:3000/api/sujets?forum_id=${id}`)
     },
     methods: {
         async fetchForum(id) {
@@ -65,7 +58,28 @@ export default {
             const response = await fetch(`http://localhost:3000/api/sujets?forum_id=${id}`);
             const data = await response.json();
             return data;
-        }
+        },
+        
+        async addSujet() {
+         const id = this.$route.params.id;
+            const sujetData = {
+                name: this.name,
+                forum_id: this.$route.params.id,
+                
+            };
+            try {
+                const response = await fetch(`http://localhost:3000/api/sujets?forum_id=${id}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(sujetData),
+                });
+                
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        },
     }    
 };
 </script>
--- Forum_Holder_Gromangin/server/api/sujets.post.ts
+++ Forum_Holder_Gromangin/server/api/sujets.post.ts
@@ -3,7 +3,7 @@ import db from "~/server/sql";
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   await db.execute(
-    "INSERT INTO sujet (name,created,forum_id) VALUES (?,?,?)",
-    [body.name, new Date(), body.forum_id]
+    "INSERT INTO sujet (name,created,forum_id,is_open) VALUES (?,?,?,?)",
+    [body.name, new Date(), body.forum_id,1]
   );
 });
