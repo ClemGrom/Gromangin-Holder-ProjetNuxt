71ee125168e5ed3b7f1eebb0d33fddb5b4a932a0
ClemGrom <100697641+ClemGrom@users.noreply.github.com>
2024-04-24 10:34:54 +0200

    Update _ws.ts

--- Forum_Holder_Gromangin/server/routes/_ws.ts
+++ Forum_Holder_Gromangin/server/routes/_ws.ts
@@ -2,17 +2,19 @@ export default defineWebSocketHandler({
   open(peer) {
     console.log("[ws] open", peer);
   },
-  message(peer, message) {
+  message(peer, rawMessage) {
+    const message = rawMessage.toString(); // Convertir le message en chaîne de caractères
     console.log("[ws] message", peer, message);
     if (message.includes("ping")) {
       peer.send("pong");
     }
     const _message = {
-      message: message.toString(), // Convertir le message en chaîne
+      message: message, // Pas besoin de le re-convertir en chaîne, car il est déjà une chaîne
     };
     console.log("[ws] message", JSON.stringify(_message));
-    peer.send(JSON.stringify(_message)); // echo back
+    peer.send(JSON.stringify(_message)); // Echo back
   },
+  
   close(peer, event) {
     console.log("[ws] close", peer, event);
   },
