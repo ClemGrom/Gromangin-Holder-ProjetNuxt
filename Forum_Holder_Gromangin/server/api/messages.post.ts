544195b5930256f53b6a6a2ee4e14b89b71a6a06
ClemGrom <100697641+ClemGrom@users.noreply.github.com>
2024-04-24 10:34:27 +0200

    Update messages.post.ts

--- Forum_Holder_Gromangin/server/api/messages.post.ts
+++ Forum_Holder_Gromangin/server/api/messages.post.ts
@@ -3,6 +3,6 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   await db.execute(
     "INSERT INTO message (author_id,content,date,sujet_id) VALUES (?,?,?,?)",
-    [body.user_id, body.content, new Date(), body.sujet_id]
+    [body.author_id, body.content, new Date(), body.sujet_id]
   );
 });
