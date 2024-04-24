31b49c2bc7b04eca31aa99122f3d6c33dc47d523
ClemGrom <100697641+ClemGrom@users.noreply.github.com>
2024-04-24 10:32:20 +0200

    websocket page message

--- Forum_Holder_Gromangin/pages/forum/sujet/[id].vue
+++ Forum_Holder_Gromangin/pages/forum/sujet/[id].vue
@@ -1,113 +1,149 @@
 <template>
     <div>
-        <v-container>
-            <v-card v-for="message in messages" :key="message.id" class="p-4 mb-4" color="blue-lighten-5">
-                <div class="d-flex">
-                    <v-card-title>{{ message.author_id }}</v-card-title>
-                    <v-card-text>
-                        <template v-if="inputMessageId === message.id">
-                            <v-text-field v-model="newMessageContent" @keyup.enter="modifierMessage"></v-text-field>
-                        </template>
-                        <template v-else>
-                            {{ message.content }}
-                        </template>
-                    </v-card-text>
-                    <v-card-subtitle>{{ new Date(message.date).toLocaleDateString('fr-FR') }}</v-card-subtitle>
-                    <v-btn color="primary" @click="showInput(message.id)">Changer msg</v-btn>
-                </div>
-            </v-card>
-            <div style="height: 60px;"></div>
-
-            <div class="d-flex" style="position: fixed; width: 100%; max-width: inherit; bottom: 0;">
-                <v-text-field v-model="content" @keyup.enter="addMessage" class="mr-2"
-                    style="flex-grow: 1;"></v-text-field>
-                <v-btn color="primary" @click="addMessage">Envoyer</v-btn>
-            </div>
-        </v-container>
+      <v-container>
+        <v-card v-for="message in messages" :key="message.id" class="p-4 mb-4" color="blue-lighten-5">
+          <div class="d-flex">
+            <v-card-title>{{ message.author_id }}</v-card-title>
+            <v-card-text>
+              <template v-if="inputMessageId === message.id">
+                <v-text-field v-model="newMessageContent" @keyup.enter="modifierMessage"></v-text-field>
+              </template>
+              <template v-else>
+                {{ message.content }}
+              </template>
+            </v-card-text>
+            <v-card-subtitle>{{ new Date(message.date).toLocaleDateString('fr-FR') }}</v-card-subtitle>
+            <v-btn color="primary"  @click="showInput(message.id)">Changer msg</v-btn>
+          </div>
+        </v-card>
+        <div style="height: 60px;"></div>
+  
+        <div class="d-flex" style="position: fixed; width: 100%; max-width: inherit; bottom: 0;">
+          <v-text-field v-model="content" @keyup.enter="sendMessage" class="mr-2" style="flex-grow: 1;"></v-text-field>
+          <v-btn color="primary" @click="sendMessage">Envoyer</v-btn>
+        </div>
+      </v-container>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  
+  export default {
     data() {
-        return {
-            messages: [],
-            error: '',
-            inputMessageId: null,
-            newMessageContent: '',
-            content: '',
-        };
+      return {
+        messages: [],
+        error: '',
+        inputMessageId: null,
+        newMessageContent: '',
+        content: '',
+        websocket: null
+      };
     },
-    async created() {
-        await this.loadMessages();
+    created() {
+      this.setupWebSocket();
+      this.loadMessages();
     },
     methods: {
-        async loadMessages() {
-            const sujetId = this.$route.params.id;
-            try {
-                const response = await fetch(`http://localhost:3000/api/messages?sujet_id=${sujetId}`);
-                const data = await response.json();
-                console.log('Received data from API:', data);
-                this.messages = data.messages;
-            } catch (error) {
-                console.error('Error loading messages:', error);
-                this.error = error.message;
-            }
-        },
-        async addMessage() {
-            const sujetId = this.$route.params.id;
-            const messageData = {
-                user_id: 1,
-                content: this.content,
-                sujet_id: sujetId,
-            };
-            try {
-                const response = await fetch(`http://localhost:3000/api/messages?sujet_id=${sujetId}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(messageData),
-                });
-                this.content = '';
-                await this.loadMessages();
-                window.scrollTo(0, document.body.scrollHeight);
-            } catch (error) {
-                console.error('Error adding message:', error);
-                this.error = error.message;
-            }
-        },
-        async modifierMessage() {
-            const messageData = {
-                id: this.inputMessageId,
-                content: this.newMessageContent,
-            };
-            try {
-                const response = await fetch(`http://localhost:3000/api/messages`, {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(messageData),
-                });
-
-                this.inputMessageId = null;
-                this.newMessageContent = '';
-                await this.loadMessages();
-            } catch (error) {
-                console.error('Error updating message:', error);
-                this.error = error.message;
-            }
-        },
-        showInput(messageId) {
-            if (this.inputMessageId === messageId) {
-                this.modifierMessage();
-            } else {
-                this.inputMessageId = messageId;
-                const message = this.messages.find(msg => msg.id === messageId);
-                this.newMessageContent = message.content;
-            }
-        },
-    },
+      setupWebSocket() {
+        this.websocket = new WebSocket('ws://localhost:3000/_ws');
+  
+        this.websocket.onopen = () => {
+          console.log('WebSocket connected');
+        };
+  
+        this.websocket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  const message = JSON.parse(data.message);
+  message.date = new Date(); 
+  console.log('Received message:', message);
+  this.messages.push(message);
 };
-</script>
+  
+        this.websocket.onerror = (error) => {
+          console.error('WebSocket error:', error);
+        };
+  
+        this.websocket.onclose = () => {
+          console.log('WebSocket disconnected');
+        };
+      },
+      async loadMessages() {
+        const sujetId = this.$route.params.id;
+        try {
+          const response = await fetch(`http://localhost:3000/api/messages?sujet_id=${sujetId}`);
+          const data = await response.json();
+          console.log('Received data from API:', data);
+          this.messages = data.messages;
+        } catch (error) {
+          console.error('Error loading messages:', error);
+          this.error = error.message;
+        }
+      },
+      async sendMessage() {
+  if (this.content.trim() === '') return;
+
+  const messageData = {
+    author_id: 1,
+    content: this.content,
+    
+    sujet_id: this.$route.params.id,
+  };
+
+  try {
+    // Envoyer le message via WebSocket
+    this.websocket.send(JSON.stringify(messageData));
+
+    // Envoyer une requête POST à l'API pour stocker le message dans la base de données
+    const response = await fetch('http://localhost:3000/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(messageData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    window.scrollTo(0, document.body.scrollHeight);
+    this.content = '';
+  } catch (error) {
+    console.error('Error sending message:', error);
+    this.error = error.message;
+  }
+},
+      async modifierMessage() {
+        const messageData = {
+          id: this.inputMessageId,
+          content: this.newMessageContent,
+        };
+        try {
+          const response = await fetch(`http://localhost:3000/api/messages`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(messageData),
+          });
+  
+          this.inputMessageId = null;
+          this.newMessageContent = '';
+          await this.loadMessages();
+        } catch (error) {
+          console.error('Error updating message:', error);
+          this.error = error.message;
+        }
+      },
+      showInput(messageId) {
+        if (this.inputMessageId === messageId) {
+          this.modifierMessage();
+        } else {
+          this.inputMessageId = messageId;
+          const message = this.messages.find(msg => msg.id === messageId);
+          this.newMessageContent = message.content;
+        }
+      },
+    },
+  };
+  </script>
+  
