2be471575b8ccd1884c7ceef7a31f702f97194bf
ClemGrom <100697641+ClemGrom@users.noreply.github.com>
2024-04-26 16:17:44 +0200

    pseudo à la place d'id

--- Forum_Holder_Gromangin/pages/forum/[id].vue
+++ Forum_Holder_Gromangin/pages/forum/[id].vue
@@ -1,175 +1,209 @@
-<template>
-  <v-container >
-   
-      <v-row justify="center" >
-          <v-col >
-            <div class="d-flex py-2" color="grey lighten-3" v-if="isAdmin">
-    <v-text-field v-model="name" label="Nom du sujet" outlined dense color="blue-grey darken-1"></v-text-field>
-    <v-text-field v-model="firstmsg" label="Premier message" outlined dense color="blue-grey darken-1"></v-text-field>
-    <v-btn small color="blue-grey darken-1" @click="addSujetetMsg">Ajouter un sujet</v-btn>
-  </div>
 
-             <v-card class="mb-2 px-4 py-2">
-                <h1 class="text-center text-primary">Sujets</h1>
-              <h2 v-if="forum && forum.length > 0">Voici les sujets du forum: {{forum[0].name}}</h2>
-              <p v-else>Aucun forum trouvé</p>
-              <p>Nombre de Sujet du forum : {{ this.sujets.length }}</p>
-              <v-list>
-                <v-card 
-    v-for="(sujet, index) in paginatedSujets" 
-    :key="sujet.id" 
-    :color="index % 2 === 0 ? 'blue-grey-lighten-5' : 'blue-grey-lighten-4'" 
-    class="mb-2"  
-    
->
-    <nuxt-link :to="`/forum/sujet/${sujet.id}`" class="noundeline d-flex " >
-        <v-card-title>
-            Sujet : {{ sujet.name }}
-        </v-card-title>
-        <v-card-text>Crée le :{{ sujet.created }}</v-card-text>
-    </nuxt-link>
-</v-card>
-</v-list>
-</v-card>
-<v-pagination
-    v-model="page"
-    :length="Math.ceil(sujets.length / sujetsPerPage)"
-    color="primary"
-  ></v-pagination>
-          </v-col>
-      </v-row>
-  
-  </v-container>
+<template>
+  <div>
+    <v-container>
+      <v-card v-for="message in messages" :key="message.id" class="p-4 mb-4" color="blue-lighten-5">
+        <div class="d-flex  align-center justify-space-between pa-3">
+  <v-card-title class="headline font-weight-bold">{{ message.name }}</v-card-title>
+  <v-card-text class="body-1">
+    <template v-if="inputMessageId === message.id">
+      <v-text-field v-model="newMessageContent" @keyup.enter="modifierMessage" filled
+        label="Modifier le message"></v-text-field>
+    </template>
+    <template v-else>
+      {{ message.content }}
+    </template>
+  </v-card-text>
+  <v-card-subtitle class="caption grey--text">{{ new Date(message.date).toLocaleDateString('fr-FR')
+    }}</v-card-subtitle>
+  <v-btn color="primary" @click="showInput(message.id)">Modifier</v-btn>
+  <v-btn color="error" v-if="isAdmin" @click="deleteMessage(message.id)" class="ml-3">Supprimer</v-btn></div>
+      </v-card>
+      <v-card v-if="messages.length === 0" class="p-4 mb-4" color="blue-lighten-5">
+        <v-card-title class="headline font-weight-bold">Aucun message dans ce forum</v-card-title>
+      </v-card>
+      <div style="height: 60px;"></div>
+
+      <div class="d-flex align-center justify-space-between pa-3"
+    style="position: fixed; width: 100%; max-width: inherit; bottom: 0; "
+    v-if="userStore.userEmail">
+    <v-text-field v-model="content" @keyup.enter="sendMessage" filled label="Tapez votre message ici..."
+      class="mr-2 flex-grow-1"></v-text-field>
+    <v-btn color="primary" @click="sendMessage">Envoyer</v-btn>
+  </div>
+    </v-container>
+  </div>
 </template>
+
 <script>
- import { useUserStore} from '../store/user';
+import { useUserStore } from '../store/user';
 
 export default {
   data() {
     return {
-      forum: null,
-      sujets: [],
-      name: '',
-      firstmsg: '',
+      messages: [],
       error: '',
-      page: 1,
-      sujetsPerPage: 20,
+      inputMessageId: null,
+      newMessageContent: '',
+      content: '',
+      websocket: null,
       isAdmin: 0,
     };
   },
-  
-  computed: {
-    paginatedSujets() {
-      const start = (this.page - 1) * this.sujetsPerPage;
-      const end = start + this.sujetsPerPage;
-      return this.sujets.slice(start, end);
-    },
+  setup() {
+    const userStore = useUserStore();
+
+    return {
+      userStore,
+      
+   
+    };
   },
   async created() {
-    const userStore = useUserStore();
-    this.isAdmin = await userStore.getAdmin();
+    const userAdmin = useUserStore();
+    this.isAdmin = await userAdmin.getAdmin();
 
-    this.loadForum();
+    this.setupWebSocket();
+    this.loadMessages();
   },
   methods: {
-    async loadForum() {
-      const forumId = this.$route.params.id;
-      try {
-        const forumResponse = await fetch(`http://localhost:3000/api/forums?id=${forumId}`);
-        if (!forumResponse.ok) {
-          throw new Error('Erreur lors du chargement du forum');
-        }
-        const forumData = await forumResponse.json();
-        this.forum = forumData.forums;
+    setupWebSocket() {
+      this.websocket = new WebSocket('ws://localhost:3000/_ws');
 
-        const sujetsResponse = await fetch(`http://localhost:3000/api/sujets?forum_id=${forumId}`);
-      if (!sujetsResponse.ok) {
-        throw new Error('Erreur lors du chargement des sujets');
-      }
-      const sujetsData = await sujetsResponse.json();
-      this.sujets = sujetsData.sujets;
-      this.page = 1; 
-      } catch (error) {
-        this.error = error.message;
-        console.error('Erreur :', error);
-      }
-    },
-    async addSujetetMsg() {
-      await this.addSujet();
-      await this.addFirstmsg();
-    },
-    async addSujet() {
-      const forumId = this.$route.params.id;
-      const sujetData = {
-        name: this.name,
-        forum_id: forumId,
+      this.websocket.onopen = () => {
+        console.log('WebSocket connected');
       };
+
+      this.websocket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        const message = JSON.parse(data.message);
+        message.date = new Date();
+        console.log('Received message:', message);
+        this.messages.push(message);
+      };
+
+      this.websocket.onerror = (error) => {
+        console.error('WebSocket error:', error);
+      };
+
+      this.websocket.onclose = () => {
+        console.log('WebSocket disconnected');
+      };
+    },
+    async loadMessages() {
+      const sujetId = this.$route.params.id;
       try {
-        const response = await fetch(`http://localhost:3000/api/sujets?forum_id=${forumId}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(sujetData),
-        });
-        this.name = '';
-        this.loadForum();
+        const response = await fetch(`http://localhost:3000/api/messagesuser?sujet_id=${sujetId}`);
+        const data = await response.json();
+        console.log('Received data from API:', data);
+        this.messages = data.messages;
       } catch (error) {
-        this.error = 'Erreur lors de l\'ajout du sujet';
-        console.error('Erreur :', error);
+        console.error('Error loading messages:', error);
+        this.error = error.message;
       }
     },
-    async addFirstmsg() {
-      const forumId = this.$route.params.id;
-      const lastSujetId = await this.getLastSujetId();
+    async deleteMessage(messageId) {
+  try {
+    const response = await fetch(`http://localhost:3000/api/messages`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: messageId }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Recharger les messages après la suppression
+    await this.loadMessages();
+  } catch (error) {
+    console.error('Error deleting message:', error);
+    this.error = error.message;
+  }
+},
+async sendMessage() {
+  if (this.content.trim() === '') return;
+  const userId= useUserStore();
+    const id = await userId.getUserId();
+
+  const messageData = {
+    author_id: id,
+    content: this.content,
+    sujet_id: this.$route.params.id,
+  };
+
+  try {
+    // Envoyer le message via WebSocket
+    this.websocket.send(JSON.stringify(messageData));
+
+    // Envoyer une requête POST à l'API pour stocker le message dans la base de données
+    const response = await fetch('http://localhost:3000/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(messageData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Recharger les messages après l'envoi
+    await this.loadMessages();
+
+    window.scrollTo(0, document.body.scrollHeight);
+    this.content = '';
+  } catch (error) {
+    console.error('Error sending message:', error);
+    this.error = error.message;
+  }
+},
+    async modifierMessage() {
       const messageData = {
-        user_id: '1',
-        content: this.firstmsg,
-        sujet_id: parseInt(lastSujetId),
+        id: this.inputMessageId,
+        content: this.newMessageContent,
       };
       try {
         const response = await fetch(`http://localhost:3000/api/messages`, {
-          method: 'POST',
+          method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify(messageData),
         });
-        this.firstmsg = '';
-        this.loadForum();
+
+        this.inputMessageId = null;
+        this.newMessageContent = '';
+        await this.loadMessages();
       } catch (error) {
-       
-        this.error = 'Erreur lors de l\'ajout du sujet';
-        console.error('Erreur :', error);
+        console.error('Error updating message:', error);
+        this.error = error.message;
       }
     },
-    async getLastSujetId() {
-      const forumId = this.$route.params.id;
+    showInput(messageId) {
+      if (this.inputMessageId === messageId) {
+        this.modifierMessage();
+      } else {
+        this.inputMessageId = messageId;
+        const message = this.messages.find(msg => msg.id === messageId);
+        this.newMessageContent = message.content;
+      }
+    },
+
+    async getNameUserMessage(id) {
       try {
-        const sujetsResponse = await fetch(`http://localhost:3000/api/sujets?forum_id=${forumId}`);
-        if (!sujetsResponse.ok) {
-          throw new Error('Erreur lors du chargement des sujets');
-        }
-        const sujetsData = await sujetsResponse.json();
-        const lastSujet = sujetsData.sujets[sujetsData.sujets.length - 1];
-        console.log(lastSujet.id);
-        return lastSujet.id;
+        const response = await fetch(`http://localhost:3000/api/messagesuser?sujet_id=${id}`);
+        const data = await response.json();
+        return data.user.name;
       } catch (error) {
-        this.error = 'Erreur lors de la récupération du dernier sujet';
-        console.error('Erreur :', error);
+        console.error('Error loading user:', error);
+        this.error = error.message;
       }
     },
   },
-  
+
 };
 </script>
-<style scoped>
-
-.noundeline {
-  text-decoration: none;
-  color:black;
-}
-.text-primary {
-    color: #0D47A1; /* Change this to your preferred color */
-}
-</style>
