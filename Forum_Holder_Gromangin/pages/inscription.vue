8ec1f0ebf222cb9f24f69e944a69ce9cdd5c2ba6
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-26 20:15:45 +0200

    meme css pour connexion et inscription

--- Forum_Holder_Gromangin/pages/inscription.vue
+++ Forum_Holder_Gromangin/pages/inscription.vue
@@ -1,18 +1,16 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" sm="8" md="4">
-        <v-card class="elevation-12">
-          <v-toolbar color="blue lighten-4">
-            <v-toolbar-title>Inscription</v-toolbar-title>
-          </v-toolbar>
+      <v-col justify="center">
+        <v-card cols="12" sm="8" md="6" >
+          <v-card-title class="headline">Inscription</v-card-title>
           <v-card-text>
             <v-form @submit.prevent="registerUser(name, email, password, passwordrepeat)">
               <v-text-field label="Nom" name="name" type="text" v-model="name" required></v-text-field>
               <v-text-field label="Email" name="email" type="email" v-model="email" required></v-text-field>
               <v-text-field label="Mot de passe" name="password" type="password" v-model="password"
                 required></v-text-field>
-              <v-text-field label="Mot de passe répété" name="passwordrepeat" type="password" v-model="passwordrepeat"
+              <v-text-field label="Confirmer votre mot de passe" name="passwordrepeat" type="password" v-model="passwordrepeat"
                 required></v-text-field>
               <v-btn color="blue lighten-4" type="submit">S'inscrire</v-btn>
             </v-form>
